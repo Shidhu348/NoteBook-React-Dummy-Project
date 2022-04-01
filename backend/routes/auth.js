659fddca99e77a719b5bf88fe986b267ff399a8f
Shidhu348 <shidhubhadra01@gmail.com>
2022-04-01 10:39:00 +0530

    Created some authincation rule

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router()
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-
+const JWT_SECRET = "SignWebToken"
 // Create a User using: POST endpoint"/api/createuser. No Login required
 router.post('/createuser', [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
@@ -22,13 +24,23 @@ router.post('/createuser', [
             return res.status(400).json({ error: "Email with this user already exits" })
         }
 
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt);
+
         // Create a new user
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
-        })
-        res.json(user);
+            password: secPass,
+        });
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SECRET);
+        res.json({ authToken });
+
 
     } catch (error) {
         console.error(error.message)
