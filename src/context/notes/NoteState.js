0086d7301a075fe9a3346f4d0ed1f4c3cce0f5e7
Shidhu348 <shidhubhadra01@gmail.com>
2022-04-02 00:26:44 +0530

    NotesItem created which is used to show all notes

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -16,9 +16,54 @@ const NoteState = (props) => {
             "_id": "6246ace417c7bd957f02b7cc",
             "user": "6246888bfe1bb1422abe858e",
             "title": "My title",
-            "description": "This is first notes",
+            "description": "This is second notes",
             "tag": "personal",
             "date": "2022-04-01T07:42:28.764Z",
+            "__v": 0
+        },
+        {
+            "_id": "6246ace417c7bd957f02b7ce",
+            "user": "6246888bfe1bb1422abe858e",
+            "title": "My title",
+            "description": "This is first notes",
+            "tag": "personal",
+            "date": "2022-04-01T07:42:28.913Z",
+            "__v": 0
+        },
+        {
+            "_id": "6246ace517c7bd957f02b7d0",
+            "user": "6246888bfe1bb1422abe858e",
+            "title": "My title",
+            "description": "This is first notes",
+            "tag": "personal",
+            "date": "2022-04-01T07:42:29.060Z",
+            "__v": 0
+        },
+        {
+            "_id": "6246ace517c7bd957f02b7d2",
+            "user": "6246888bfe1bb1422abe858e",
+            "title": "My title",
+            "description": "This is first notes",
+            "tag": "personal",
+            "date": "2022-04-01T07:42:29.215Z",
+            "__v": 0
+        },
+        {
+            "_id": "6246ace517c7bd957f02b7d0",
+            "user": "6246888bfe1bb1422abe858e",
+            "title": "My title",
+            "description": "This is first notes",
+            "tag": "personal",
+            "date": "2022-04-01T07:42:29.060Z",
+            "__v": 0
+        },
+        {
+            "_id": "6246ace517c7bd957f02b7d2",
+            "user": "6246888bfe1bb1422abe858e",
+            "title": "My title",
+            "description": "This is first notes",
+            "tag": "personal",
+            "date": "2022-04-01T07:42:29.215Z",
             "__v": 0
         }
     ]
