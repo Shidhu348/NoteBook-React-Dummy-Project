b59e731f4ec956cf3c43680418a222a9f4005817
Shidhu348 <shidhubhadra01@gmail.com>
2022-04-01 21:55:30 +0530

    context api created

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import './App.css';
 import {
-  BrowserRouter as Router, Routes, Route
+  BrowserRouter as Router,
+  Routes,
+  Route
 } from "react-router-dom";
-import Navbar from './components/Navbar'
-import Home from './components/Home';
+import Navbar from './components/Navbar';
 import About from './components/About';
-
+import NoteState from './context/notes/NoteState';
+import Home from './components/Home'
 
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/about" element={<About />} />
-        </Routes>
-      </Router>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+          </Routes>
+        </Router>
+      </NoteState>
     </>
   );
 }
---
+++ src/context/notes/NoteState.js
@@ -0,0 +1,28 @@
+import NoteContext from "./noteContext";
+import { useState } from "react";
+
+const NoteState = (props) => {
+    const s1 = {
+        "name": "Shidhu",
+        "class": "b8"
+    }
+
+    const [state, setState] = useState(s1)
+
+    const update = () => {
+        setTimeout(() => {
+            setState({
+                "name": "Anik",
+                "class": "C8"
+            })
+        }, 1000);
+    }
+
+    return (
+        <NoteContext.Provider value={{ state, update }}>
+            {props.children}
+        </NoteContext.Provider>
+    )
+}
+
+export default NoteState;
