1b5e304db0c7f536272b37dc88f57d32dd528c02
Shidhu348 <shidhubhadra01@gmail.com>
2022-04-02 15:44:26 +0530

    Feat: delete functionality added
    Desc: upon clicking delete icon the item will be deleted from the client side only

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -89,13 +89,18 @@ const NoteState = (props) => {
 
 
     // Delete a Note
-    const deleteNote = () => {
-
+    const deleteNote = (id) => {
+        // Todo API call
+        console.log("Deleting the note with id" + id)
+        const newNotes = notes.filter((note) => {
+            return note._id !== id
+        })
+        setNotes(newNotes)
     }
 
 
     // Edit a note
-    const editNote = () => {
+    const editNote = (id, title, description, tag) => {
 
     }
 
