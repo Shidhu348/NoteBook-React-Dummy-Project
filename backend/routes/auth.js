d0094fc06c3a8327737e2701b546fd42b19456cc
Shidhu348 <shidhubhadra01@gmail.com>
2022-04-02 21:31:34 +0530

    Feat: Sign Up functionality added
    Desc: SignUp added and linked with backend. Alert prototype created for now will be enhanced later

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -16,16 +16,19 @@ router.post('/createuser', [
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password must be atleast 5 characters').isLength({ min: 5 })
 ], async (req, res) => {
+    let success = false;
     // If there are errors return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+        success = false;
+        return res.status(400).json({ success, errors: errors.array() });
     }
     // Check whether the user with this email exits already
     try {
         let user = await User.findOne({ email: req.body.email });
         if (user) {
-            return res.status(400).json({ error: "Email with this user already exits" })
+            success = false;
+            return res.status(400).json({ success, error: "Email with this user already exits" })
         }
 
         const salt = await bcrypt.genSalt(10);
@@ -42,8 +45,9 @@ router.post('/createuser', [
                 id: user.id
             }
         }
+        success = true;
         const authToken = jwt.sign(data, JWT_SECRET);
-        res.json({ authToken });
+        res.json({ success, authToken });
 
 
     } catch (error) {
--- src/components/Login.js
+++ src/components/Login.js
@@ -34,18 +34,20 @@ const Login = () => {
 
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div className="mb-3">
-                <label htmlFor="email" className="form-label">Email address</label>
-                <input type="email" className="form-control" name="email" id="email" aria-describedby="emailHelp" onChange={onChange} value={credentials.email} />
-                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="password" className="form-label">Password</label>
-                <input type="password" name="password" className="form-control" id="password" onChange={onChange} value={credentials.password} />
-            </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
+        <div className="container my-5">
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email address</label>
+                    <input type="email" className="form-control" name="email" id="email" aria-describedby="emailHelp" onChange={onChange} value={credentials.email} />
+                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" name="password" className="form-control" id="password" onChange={onChange} value={credentials.password} />
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </div>
     )
 }
 
